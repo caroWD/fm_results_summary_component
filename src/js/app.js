dffa59850a5a2d78234b5a462ef9150dede83dfe
caroDesign <juego.fuego@gmail.com>
2025-01-09 20:38:31 -0500

    Added JS interactivity

--- src/js/app.js
+++ src/js/app.js
@@ -21,5 +21,43 @@
     header.classList.remove('animate__fadeInDown');
     header.classList.add('animate__fadeInLeft');
   }
-  window.onresize = resize;
 })();
+
+const calculateTotal = (data) => {
+  let subtotal = 0;
+  let total = 0;
+  data.forEach(score => subtotal += score.score);
+  return total = parseInt(subtotal / data.length);
+}
+
+const showScore = (place, data) => {
+  let counter = 0;
+  setInterval(() => {
+    counter++;
+    if (counter < data) {
+      place.innerHTML = counter;
+    } else {
+      place.innerHTML = data;
+      return
+    }
+  }, 50);
+}
+
+const urlOrigin = window.location.origin;
+const scoreTotal = document.querySelector('[data-score="total"]');
+const itemScores = [...document.querySelectorAll('[data-score="score"]')];
+
+fetch(`${urlOrigin}/src/data/data.json`)
+  .then(response => response.json())
+  .then(data => {
+    const total = calculateTotal(data);
+    showScore(scoreTotal, total);
+    itemScores.forEach((score, i) => {
+      let item = score.querySelector('strong');
+      let titleScore = score.querySelector('[data-score="title-score"]');
+      let urlIcon = data[i].icon.replace('./assets/images', '../img');
+      titleScore.innerText = data[i].category;
+      showScore(item, data[i].score);
+      score.classList.add(`before:content-[url('${urlIcon}')]`)
+    })
+  })
